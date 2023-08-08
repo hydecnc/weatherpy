a73f8ac07c003d0039acd18c918e104d4572eb67
Hyde <seokjinyoo0716@gmail.com>
2023-08-08 13:54:07 -0600

    major refactoring, updated requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import typer
-from typing_extensions import Annotated, Optional
-
-# from rich import print
-from weather import get_weather
+from typing_extensions import Annotated
+from weather import Weather
 
 
 app = typer.Typer()
@@ -10,21 +8,18 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    verbosity: bool = typer.Option(
-        False,
-        "--verbose",
-        "-v",
-        help="Get more information (High / Low, Wind, Humidity, Dew Point, Pressure, UV Index, Visibility, Moon Phase)",
-    ),
-    date: str = typer.Option(
-        "today",
-        "--date"
-        "-d"
-    ),
-    location: str = "",
-    unit: str = "c",
+    date: Annotated[str, typer.Option("--date", "-d", help="Date range of the weather.", rich_help_panel="Options")] = "today",
+    location: Annotated[str, typer.Option("--location", "-l", help="The desired locaiton of the weather. Deafults to user's ip location if not provided.", rich_help_panel="Options")] = "",
+    unit: Annotated[str, typer.Option("--unit", "-u", help="Unit of the weather (celcius/farenheit).", rich_help_panel="Options")] = "c",
+    verbose: Annotated[bool, typer.Option("--verbose", "-v", help="Get more information (High / Low, Wind, Humidity, Dew Point, Pressure, UV Index, Visibility, Moon Phase).", rich_help_panel="Options")] = False,
 ):
-    print(f"{get_weather(date, location, unit, verbosity)}")
+    """
+    Get weather of the desired location, date range, and units.
+    
+    Defaults to today's weather in user location in celcius. 
+    """
+    weather = Weather(location=location, unit=unit)
+    print(f"{weather.get_weather(date=date, verbose=verbose)}")
 
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -1,90 +1,89 @@
 from requests_html import HTMLSession, HTMLResponse
 from geopy.geocoders import Nominatim
 
-session = HTMLSession()
 
-def get_weather(
-    date: str = "today", location: str = "", unit: str = "c", verbose: bool = False
-) -> str:
-    """gets tempreature of given location (at given) in the provided unit
+class Weather:
+    def __init__(self, location: str = "", unit: str = "c") -> None:
+        self.session = HTMLSession()
+        
+        self.location = self.get_loc(location)
+        print(f"Looking for weather at {self.location}")
 
-    Args:
-        date (str, optional): date range of the weather. Defaults to "today".
-        location (str, optional): location ex) "New York". Defaults to "".
-        unit (str, optional): unit either Celcius(c) or Farenheit(f). Defaults to "c".
-        verbose (bool, optional): whether to get extra info (High / Low, Wind, Humidity, Dew Point, Pressure, UV Index, Visibility, Moon Phase) or not. Defaults to False.
+        url = f"https://weather.com/weather/today/l/{self.location}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Accept-Langauge": "en-US,en;q=0.9",
+        }
+        params = {
+            "unit": "m" if unit == "c" else "e",
+        }
 
-    Returns:
-        str: formatted string of the required data
-    """
+        self.results = self.session.get(
+            url,
+            headers=headers,
+            params=params,
+        )
 
-    location = get_loc(location)
-    print(f"Looking for weather at {location}")
+    def get_weather(
+        self, date: str = "today", verbose: bool = False
+    ) -> str:
+        """gets tempreature of given location (at given) in the provided unit
 
-    url = f"https://weather.com/weather/today/l/{location}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "Accept-Langauge": "en-US,en;q=0.9",
-    }
-    params = {
-        "unit": "m" if unit == "c" else "e",
-    }
+        Args:
+            date (str, optional): date range of the weather. Defaults to "today".
+            verbose (bool, optional): whether to get extra info (High / Low, Wind, Humidity, Dew Point, Pressure, UV Index, Visibility, Moon Phase) or not. Defaults to False.
 
-    results = session.get(
-        url,
-        headers=headers,
-        params=params,
-    )
+        Returns:
+            str: formatted string of the required data
+        """
 
-    temperature = results.html.find(
-        "span.CurrentConditions--tempValue--MHmYY", first=True
-    ).text
-    feel_like_tempreature = results.html.find(
-        "span.TodayDetailsCard--feelsLikeTempValue--2icPt", first=True
-    ).text
-    # other = results.html.find("div.TodayDetailsCard--detailsContainer--2yLtL", first=True).find("div")
-    # for ot in other:
-    #     print(ot.find("div"))
-    if verbose:
-        other_data = get_other_data(results)
-        return f"Temperature: {temperature}\nFeels Like: {feel_like_tempreature}\nOther Data: {other_data}"
-    else:
-        return f"Temperature: {temperature}\nFeels Like: {feel_like_tempreature}"
+        temperature = self.results.html.find(
+            "span.CurrentConditions--tempValue--MHmYY", first=True
+        ).text
+        feel_like_tempreature = self.results.html.find(
+            "span.TodayDetailsCard--feelsLikeTempValue--2icPt", first=True
+        ).text
 
-
-def get_other_data(results: HTMLResponse) -> dict:
-    """get extra data if verbose flag is used
-
-    Args:
-        results (HTMLResponse): the HTMLSession
-
-    Returns:
-        dict: dictionary of the extra data
-    """
-    other_data = {}
-    other_data_names = results.html.find("div.WeatherDetailsListItem--label--2ZacS")
-    other_data_values = results.html.find("div.WeatherDetailsListItem--wxData--kK35q")
-
-    for data in zip(other_data_names, other_data_values):
-        other_data[data[0].text] = data[1].text
-
-    return other_data
+        if verbose:
+            other_data = self.get_extra_data()
+            return f"Temperature: {temperature}\nFeels Like: {feel_like_tempreature}\nOther Data: {other_data}"
+        else:
+            return f"Temperature: {temperature}\nFeels Like: {feel_like_tempreature}"
 
 
-def get_loc(location: str = "") -> str:
-    """Get user's current location using curl if a location is not defined
+    def get_extra_data(self) -> dict:
+        """get extra data if verbose flag is used
 
-    Args:
-        location (str, optional): location from user. Defaults to "".
+        Returns:
+            dict: dictionary containg the extra data
+        """
+        extra_data = {}
+        extra_data_names = self.results.html.find("div.WeatherDetailsListItem--label--2ZacS")
+        extra_data_values = self.results.html.find("div.WeatherDetailsListItem--wxData--kK35q")
 
-    Returns:
-        str: location in a form of "lat,long"
-    """
-    if location == "":
-        # loc = subprocess.run(["curl", "ipinfo.io/loc"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
-        loc = session.get("http://ipinfo.io/json").json()["loc"]
-        return loc
+        for data in zip(extra_data_names, extra_data_values):
+            if data[0].text == "Wind" or data[0].text == "Pressure":
+                wanted_value = data[1].text.replace("\xa0", " ").split("\n")[1]
+                extra_data[data[0].text] = wanted_value
+                continue
+            extra_data[data[0].text] = data[1].text
 
-    geolocator = Nominatim(user_agent="MyApp")
-    loc = geolocator.geocode(location)
-    return f"{loc.latitude},{loc.longitude}"
+        return extra_data
+
+
+    def get_loc(self, location: str = "") -> str:
+        """Get user's current location using curl if a location is not defined
+
+        Args:
+            location (str, optional): location from user. Defaults to "".
+
+        Returns:
+            str: location in a form of "lat,long"
+        """
+        if location == "":
+            # loc = subprocess.run(["curl", "ipinfo.io/loc"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+            loc = self.session.get("http://ipinfo.io/json").json()["loc"]
+            return loc
+        geolocator = Nominatim(user_agent="MyApp")
+        loc = geolocator.geocode(location)
+        return f"{loc.latitude},{loc.longitude}"
